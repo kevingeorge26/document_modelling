ca9e4de0000790b9950e45d74ba1f22bfb60fb20
kevin varkey <kevingeorge@gmail.com>
2013-01-31 19:48:39 -0600

    ngrams and wordcount

--- Basic_PreProcessing.py
+++ Basic_PreProcessing.py
@@ -16,25 +16,34 @@ def get_rfc_text():
     pass
 
 
-def process_rfc():
+def calculate_word_freq(filename,number_of_words):
+    
     """ code to get the word count in the protocol """
-    with open("test.txt") as f:
+    """ prints the top number_of_words"""
+    with open(filename) as f:
         for i in f:
             with_stop_words = i.translate(trantab).lower()
             without_stop_words = filter(lambda x: x not in stop_words, with_stop_words.split(" "))
             
             for word in without_stop_words:
                 final_list[word]+=1
-                
+    counter = 0            
     for words in sorted(final_list, key=final_list.get, reverse=True):
         print final_list[words] , words
+        
+        if counter > number_of_words:
+            break
+        
+        counter+=1
                 
 
-def calculate_bigrams():
-    """ print top 10 bigrams """
+def calculate_bigrams(filename,number_bigrams):
+    """ print top number_bigrams bigrams """
     bigram_measures = nltk.collocations.BigramAssocMeasures()
-    finder = BigramCollocationFinder.from_words(open("test.txt").read().split())
-    print finder.nbest(bigram_measures.pmi, 10)  
+    finder = BigramCollocationFinder.from_words(open(filename).read().split())
+    scored = finder.score_ngrams( bigram_measures.likelihood_ratio  )
+    scored.sort(key = lambda x:x[1],reverse=True)
+    print scored[:number_bigrams]
         
 
 if __name__ == '__main__':
@@ -44,8 +53,9 @@ if __name__ == '__main__':
     
     trantab = maketrans(punctuation, " " * len(punctuation))
     stop_words  = set(open("stop_words.txt").read().split(","))
-#    process_rfc()
-    calculate_bigrams()
+    
+    calculate_word_freq(filename="test.txt",number_of_words = 10)
+    calculate_bigrams(filename="test.txt",number_bigrams=10)
     
    
     
